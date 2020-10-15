d20f2c75a56c12d7d663d36c209e346557a084cb
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-15 16:37:10 +0200

    fatto

--- js/javascript.js
+++ js/javascript.js
@@ -4,19 +4,31 @@ var indirizzi=[ 'luca@gmail.it', 'mario@gmail.it', 'giovanni@gmail.it'];
 var utente=prompt('inserisci il tuo indirizzo gmail').toLocaleLowerCase();
 
 
-if (utente == 'luca@gmail.it'){
-    console.log('email corretta')
-    alert('email correta benvenuto/a')
+// if (utente == 'luca@gmail.it'){
+//     console.log('email corretta')
+//     alert('email correta benvenuto/a')
+// }
+// else if (utente == 'mario@gmail.it'){
+//     console.log('email corretta')
+//     alert('email correta benvenuto/a')
+// }
+// else if (utente == 'giovanni@gmail.it'){
+//     console.log('email corretta')
+//     alert('email correta benvenuto/a')
+// }
+// else{
+//     console.log('email non corretta')
+//     alert('email non correta riprova')
+// }
+for(var i =0; i<indirizzi.length; i++){
+    if(utente==indirizzi[i]){
+        console.log('email corretta')
+        alert('email correta benvenuto/a')
+    }
+    else{
+        console.log('email non corretta')
+        alert('email non correta riprova')
+    }
 }
-else if (utente == 'mario@gmail.it'){
-    console.log('email corretta')
-    alert('email correta benvenuto/a')
-}
-else if (utente == 'giovanni@gmail.it'){
-    console.log('email corretta')
-    alert('email correta benvenuto/a')
-}
-else{
-    console.log('email non corretta')
-    alert('email non correta riprova')
-}
+// esecizio DADI--------------------------
+
