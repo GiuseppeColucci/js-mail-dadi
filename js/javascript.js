bc26eb73c7c8feecaadcead5d1527a067433c4ff
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-15 15:48:23 +0200

    esercizio email finito

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,22 @@
 // eserzio EMAIL---------
 
 var indirizzi=[ 'luca@gmail.it', 'mario@gmail.it', 'giovanni@gmail.it'];
-var utente=prompt('inserisci il tuo indirizzo gmail');
+var utente=prompt('inserisci il tuo indirizzo gmail').toLocaleLowerCase();
 
 
-if (utente == indirizzi){
-    console.log('ciao 222222222')
+if (utente == 'luca@gmail.it'){
+    console.log('email corretta')
+    alert('email correta benvenuto/a')
+}
+else if (utente == 'mario@gmail.it'){
+    console.log('email corretta')
+    alert('email correta benvenuto/a')
+}
+else if (utente == 'giovanni@gmail.it'){
+    console.log('email corretta')
+    alert('email correta benvenuto/a')
+}
+else{
+    console.log('email non corretta')
+    alert('email non correta riprova')
 }
