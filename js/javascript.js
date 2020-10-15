80e5941d12c2f5d0ea11f4ade06b28b8270c660f
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-15 15:10:56 +0200

    mmmmmm

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,9 @@
 // eserzio EMAIL---------
-console.log('ciao ciao')
-var indirizzi=[ 'luca@gimai.it', 'mario@gmail.it', 'giovanni@gmail.it'];
+
+var indirizzi=[ 'luca@gmail.it', 'mario@gmail.it', 'giovanni@gmail.it'];
 var utente=prompt('inserisci il tuo indirizzo gmail');
 
-if (indirizzi == utente){
-    console.log('la tua meail è correta entra pure')
+
+if (utente == indirizzi){
+    console.log('ciao 222222222')
 }
