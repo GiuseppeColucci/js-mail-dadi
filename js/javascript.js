ecc2176f6b62be27209e16a0c5f020187b11c2ac
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-15 14:57:50 +0200

    manca qualcosa?

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+// eserzio EMAIL---------
+console.log('ciao ciao')
+var indirizzi=[ 'luca@gimai.it', 'mario@gmail.it', 'giovanni@gmail.it'];
+var utente=prompt('inserisci il tuo indirizzo gmail');
+
+if (indirizzi == utente){
+    console.log('la tua meail è correta entra pure')
+}
