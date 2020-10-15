20e78946f077a45a190786fbb1d05ebc576db755
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-15 20:02:04 +0200

    finito

--- js/javascript.js
+++ js/javascript.js
@@ -2,27 +2,7 @@
 
 var indirizzi=[ "luca@gmail.it", "mario@gmail.it", "giovanni@gmail.it"];
 var utente=prompt('inserisci il tuo indirizzo gmail').toLocaleLowerCase();
-
-
-// if (utente == 'luca@gmail.it'){
-//     console.log('email corretta')
-//     alert('email correta benvenuto/a')
-// }
-// else if (utente == 'mario@gmail.it'){
-//     console.log('email corretta')
-//     alert('email correta benvenuto/a')
-// }
-// else if (utente == 'giovanni@gmail.it'){
-//     console.log('email corretta')
-//     alert('email correta benvenuto/a')
-// }
-// else{
-//     console.log('email non corretta')
-//     alert('email non correta riprova')
-// }
 var accountEsistente=false;
-
-
 
 for(var i =0; i<indirizzi.length; i++){
     if(utente==indirizzi[i]){
@@ -38,3 +18,7 @@ if (accountEsistente == true){
 else {
     alert('email non esistente');
 }
+
+
+
+
